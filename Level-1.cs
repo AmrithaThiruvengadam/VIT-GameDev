9b17f104899e9bc0edf405fe7d7da4c4799c9210
AmrithaThiruvengadam <73210746+AmrithaThiruvengadam@users.noreply.github.com>
2020-11-26 14:47:47 +0530

    Add files via upload

---
+++ Level-1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking; 
+using System.Collections;
+
+ 
+public class MyBehaviour : MonoBehaviour {
+    void Start() {
+        StartCoroutine(GetText());
+    }
+ 
+    IEnumerator GetText() {
+        UnityWebRequest www = new UnityWebRequest("https://s3-ap-southeast-1.amazonaws.com/cdn.hitwicket.co/sample_user_data.json ");
+        www.downloadHandler = new DownloadHandlerBuffer();
+        yield return www.SendWebRequest();
+ 
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+ 
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+}
+    }
+}
